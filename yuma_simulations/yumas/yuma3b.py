fix normalization

yuma_simulations/yumas/yuma3b.py
@@ -79,7 +79,7 @@ def Yuma3B(
     B = decay * B_old + purchase
     B = torch.min(B, capacity_per_bond)  # Enforce capacity constraints
 
-    B_scaled = B / B.sum(dim=1, keepdim=True)
+    B_scaled = B / B.sum(dim=0, keepdim=True)
 
     # === Dividends Calculation ===
     D = (B_scaled * I).sum(dim=1)

yuma_simulations/yumas/yuma4b.py
@@ -97,7 +97,7 @@ def Yuma4B(
     B = B_decayed + purchase
     B = torch.clamp(B, max=1.0)
 
-    B_scaled = B / B.sum(dim=1, keepdim=True)
+    B_scaled = B / B.sum(dim=0, keepdim=True)
 
     # === Dividends Calculation ===
     total_bonds_per_validator = (B_scaled * I).sum(